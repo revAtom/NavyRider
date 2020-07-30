8dd52b6f082357128427c053167df3c579ee8855
Liubomyyr <liubko81@gmail.com>
2020-07-30 11:59:15 +0300

    #Current Project

---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerMovement : PlayerInput
+{
+    private Rigidbody playerRb;
+
+    public float forwardAccel = 1, maxSpeed, turnsStrength = 20, mulitipier = 1000;
+
+    void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        transform.localRotation = Quaternion.Euler(transform.rotation.eulerAngles
+            + new Vector3(0, turnAngle * turnsStrength * Time.fixedDeltaTime, 0));
+
+
+
+        playerRb.AddForce(Vector3.left * forwardAccel * mulitipier * Time.fixedDeltaTime, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    protected int turnAngle = 1;
+
+    public void Update()
+    {
+        PlayerTurns();
+    }
+
+    void PlayerTurns()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            turnAngle = -turnAngle;
+        }
+    }
+}
